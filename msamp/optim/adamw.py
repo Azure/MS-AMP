ec68e7198acfadab1e1ade28a3004e11616b48b6
yuxiangyang <yuxiangyang@microsoft.com>
2023-03-11 14:22:38 +0000

    DsLBAdam

--- msamp/optim/adamw.py
+++ msamp/optim/adamw.py
@@ -261,3 +261,33 @@ class LBAdam(LBAdamW):
             **kwargs
         )
         self.use_adam = True
+
+
+class DsLBAdam(LBAdamW):
+    """An low-bits adam optimizer used as basic optimizer in DeepSpeed."""
+    def __init__(
+        self,
+        params,
+        lr=1e-3,
+        bias_correction=True,
+        betas=(0.9, 0.999),
+        eps=1e-8,
+        adam_w_mode=True,
+        weight_decay=0.,
+        amsgrad=False,
+        set_grad_none=True,
+        **kwargs
+    ):
+        """Constructor. See LBAdamW class docstring for details."""
+        super().__init__(
+            params,
+            lr=lr,
+            bias_correction=bias_correction,
+            betas=betas,
+            eps=eps,
+            weight_decay=weight_decay,
+            amsgrad=amsgrad,
+            **kwargs
+        )
+        self.use_adam = not adam_w_mode
+        self.set_grad_none = set_grad_none
