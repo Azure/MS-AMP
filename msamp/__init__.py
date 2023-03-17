8c1171cb6bfb20db9445ca149143671cdf945b83
yuxiangyang <yuxiangyang@microsoft.com>
2023-03-17 08:44:42 +0000

    Add msamp.initialize api and unit test

--- msamp/__init__.py
+++ msamp/__init__.py
@@ -8,4 +8,70 @@ from msamp.nn import clip_grad_norm_
 __version__ = '0.1.0'
 __author__ = 'Microsoft'
 
-__all__ = ['clip_grad_norm_']
+__all__ = ['clip_grad_norm_', 'initialize']
+
+import torch
+
+from msamp.nn import LinearReplacer
+from msamp.optim import LBAdam, LBAdamW
+
+opt_levels = ['O1', 'O2']
+
+
+def initialize(model, optimizer=None, opt_level='O1'):    # noqa: C901
+    """Initialize your model, optimizer according to the optimization level.
+
+    msamp.initialize() should be called after you have finished constructing your model and optimizer.
+    Currently, msamp.initialize() should be called only once.
+
+    Args:
+        model (torch.nn.Module): Model to cast.
+        optimizer (torch.optim.Optimizer): Optimizer to cast.
+        opt_level (str): Optimization level. Currently supports 'O1', 'O2'.
+            opt_level || Gemm || Communication || Weight || Weight Gradient || Optimizer States
+            'O1'      || fp8  || fp8           || fp16   || fp8             || fp32 + FP32
+            'O2'      || fp8  || fp8           || fp16   || fp8             || fp8 + fp16
+    Return:
+        return the casted model and optimizer.
+    """
+    if not isinstance(model, torch.nn.Module):
+        raise ValueError('Model must be an instance of torch.nn.Module')
+
+    if opt_level not in opt_levels:
+        raise ValueError('Invalid optimization level. Please choose from {}'.format(opt_levels))
+
+    if not isinstance(optimizer, (torch.optim.AdamW, torch.optim.Adam)):
+        raise ValueError('Optimizer {} is not supported in optimization level {}'.format(optimizer, opt_level))
+
+    cast_model = LinearReplacer.replace(model)
+    if not optimizer:
+        # default optimizer.
+        optimizer = LBAdamW(cast_model.parameters())
+
+    default_args = optimizer.defaults
+
+    exp_avg_dtype = torch.float32
+    exp_avg_sq_dtype = torch.float32
+    if opt_level == 'O2':
+        exp_avg_dtype = torch.uint8
+        exp_avg_sq_dtype = torch.float16
+
+    default_args['exp_avg_dtype'] = exp_avg_dtype
+    default_args['exp_avg_sq_dtype'] = exp_avg_sq_dtype
+
+    # currently, we don't support foreach and capturable.
+    if 'foreach' in default_args:
+        del default_args['foreach']
+    if 'capturable' in default_args:
+        del default_args['capturable']
+
+    parameters = cast_model.parameters()
+    cast_optimizer = None
+    if isinstance(optimizer, torch.optim.Adam):
+        cast_optimizer = LBAdam(parameters, **default_args)
+    elif isinstance(optimizer, torch.optim.AdamW):
+        cast_optimizer = LBAdamW(parameters, **default_args)
+
+    cast_optimizer.set_model(cast_model)
+
+    return cast_model, cast_optimizer
---
+++ tests/test_initialize.py
@@ -0,0 +1,46 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+"""Tests for msamp.initialize."""
+
+import unittest
+import torch
+
+import msamp
+from msamp.nn.linear import FP8Linear
+from msamp.optim import LBAdamW, LBAdam
+from tests.helper import decorator
+
+
+class InitializeTestCase(unittest.TestCase):
+    """Test initialize function in msamp."""
+    def setUp(self):
+        """Hook method for setting up the test fixture before exercising it."""
+        torch.manual_seed(1000)
+
+    def tearDown(self):
+        """Hook method for deconstructing the test fixture after testing it."""
+        pass
+
+    @decorator.cuda_test
+    def test_initialize(self):
+        """Test initialize function."""
+        model = torch.nn.Linear(4, 4)
+        with self.assertRaises(ValueError):
+            msamp.initialize(model, torch.optim.AdamW(model.parameters()), 'O0')
+        with self.assertRaises(ValueError):
+            msamp.initialize(model, torch.optim.SGD(model.parameters()))
+
+        for opt_level in ['O1', 'O2']:
+            model = torch.nn.Linear(4, 4)
+            optimizer = torch.optim.Adam(model.parameters())
+            model, optimizer = msamp.initialize(model, optimizer, opt_level)
+            assert isinstance(model, FP8Linear)
+            assert isinstance(optimizer, LBAdam)
+
+        for opt_level in ['O1', 'O2']:
+            model = torch.nn.Linear(4, 4)
+            optimizer = torch.optim.AdamW(model.parameters())
+            model, optimizer = msamp.initialize(model, optimizer, opt_level)
+            assert isinstance(model, FP8Linear)
+            assert isinstance(optimizer, LBAdamW)
