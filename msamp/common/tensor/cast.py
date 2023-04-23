[Bug Fixed] Fix inf/nan weight gradient checker (#58)
**Description**
When training a model, the function
[`_amp_foreach_non_finite_check_and_unscale_`](https://github.com/Azure/MS-AMP/blob/main/msamp/common/tensor/tensor.py#L750)
is called to check whether INF/NAN exists in weight gradients. For the
weight gradients stored in ScalingTensor, its INF/NAN is checked by
`scale_inv` and `amax[0]` (tensor.py#L790 and L795).

However, in the previous code, `amax[0]` is not synchronized across
multiple GPUs. It leads to different states whether INF/NAN exists
across multiple GPUs. The inconsistent behaviour drops the accuracy of
model.

In this PR, I fix the bug to keep the consistence of `amax[0]` across
GPUs. Here is the performance table. Fix #32

- Validation top-1 accuracy on ImageNet-1k

Model | Device | top-1 Acc. (%)
---|---|---
DeiT-S | H100 | 79.9
Swin-T | H100 | 81.1 

Baseline: DeiT-S: 79.9, Swin-T: 81.2

**Major Revision**
- Synchronize `meta.amax[0]` rather than `meta.scale`. Since `max(NAN,
3) = 3` in NCCL AllReduce, we convert all NAN to INF before NCCL
AllReduceMax.
- Create the tensor `arg_amax` to avoid
`TransformerEngineWrapper.cast_to_fp8` to re-write `meta.amax[0]`.
- Set the maximum scaling factor for `cast_to_fp16`.
- re-write the update of scaling factors in `msamp/optim/optimizer.py`
- unit-test for the cast of ScalingTensor with NAN or INF.

msamp/common/tensor/cast.py
@@ -6,7 +6,7 @@
 import torch
 import torch.distributed as dist
 
-from msamp.common.dtype import Dtypes
+from msamp.common.dtype import Dtypes, Floating
 from msamp.common.utils import DistUtil
 from msamp.common.utils import TransformerEngineWrapper
 
@@ -32,15 +32,23 @@ class TypeCast:
         in_time = meta.is_in_time_scaling()
         if in_time:
             meta.amax[0] = input.abs().max()
-            meta.reset_scaling_factor()
+            # create a temporary tensor to avoid cast_to_fp8 to modify meta.amax[0]
+            arg_amax = torch.zeros_like(meta.amax[0])
+        else:
+            arg_amax = meta.amax[0]
         if sync:
+            # convert NAN to INF since NCCL-ReduceMax ignores NAN
+            # notice: nan and posinf must be INF
+            meta.amax[0].nan_to_num_(nan=torch.inf, posinf=torch.inf)
             world_size = DistUtil.get_world_size()
             if world_size > 1:
-                dist.all_reduce(meta.scale, op=dist.ReduceOp.MIN)
+                dist.all_reduce(meta.amax[0], op=dist.ReduceOp.MAX)
+        if in_time or sync:
+            meta.reset_scaling_factor()
         input_fp8 = TransformerEngineWrapper.cast_to_fp8(
             input.view(1, -1),
             meta.scale,
-            meta.amax[0],
+            arg_amax,
             meta.scale_inv,
             meta.qtype,
         )
@@ -62,13 +70,17 @@ class TypeCast:
         """
         meta.amax[0] = input.abs().max()
         in_time = meta.is_in_time_scaling()
-        if in_time:
-            # notice: we scale the tensor with qtype FP8-E4M3.
-            meta.reset_scaling_factor(qtype=Dtypes.kfloat8_e4m3)
         if sync:
+            # convert NAN to INF since NCCL-ReduceMax ignores NAN
+            # notice: nan and posinf must be INF
+            meta.amax[0].nan_to_num_(nan=torch.inf, posinf=torch.inf)
             world_size = DistUtil.get_world_size()
             if world_size > 1:
-                dist.all_reduce(meta.scale, op=dist.ReduceOp.MIN)
+                dist.all_reduce(meta.amax[0], op=dist.ReduceOp.MAX)
+        if in_time or sync:
+            # notice: we scale the tensor with qtype FP8-E4M3.
+            meta.reset_scaling_factor(qtype=Dtypes.kfloat8_e4m3)
+        meta.scale.clamp_(max=Floating.qfp_max[meta.qtype])
 
         meta.scale_inv.data.copy_(torch.reciprocal(meta.scale))    # scale_inv = 1 / scale
         input_fp16 = (input * meta.scale).to(torch.float16)

msamp/optim/optimizer.py
@@ -91,15 +91,25 @@ class LBOptimizer(Optimizer):
             amax_counters = meta['amax_counters']
             fp_max = Floating.qfp_max[qtype]
             # compute scaling factor before rolling amaxs
-            sf = ScalingMeta.compute_scaling_factor(amaxs.max(1).values, scales, fp_max, margin)
 
             mask_valid = torch.isfinite(amaxs[:, 0])
             mask_inf_nan = ~mask_valid
+            # filter out inf and nan from amaxs
+            amaxs[mask_inf_nan, 0] = 0
+
+            sf = ScalingMeta.compute_scaling_factor(amaxs.max(1).values, scales, fp_max, margin)
+
+            # shift window
             amaxs.copy_(amaxs.roll(1, dims=1))
+            # set 0 for the first element in the window
             amaxs[:, 0] = 0
+
             amax_counters += 1
+            # reset counter when meeting inf or nan
             amax_counters[mask_inf_nan] = 0
-            scales[mask_valid] = sf[mask_valid]
+
+            # update scaling factors
+            scales.copy_(sf)
 
     def state_dict(self):
         r"""Returns the state of the optimizer as a :class:`dict`.