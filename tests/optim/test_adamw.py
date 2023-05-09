e2ada259a9eaa786101523e92a6c3321db4535f7
wkcn <wkcn@live.cn>
2023-05-09 04:38:42 +0000

    [bug fixed] fix all_reduce_grads when training more than one models

--- tests/optim/test_adamw.py
+++ tests/optim/test_adamw.py
@@ -9,7 +9,7 @@ import torch
 
 from msamp.common.dtype import Dtypes
 from msamp.optim import LBAdamW, LBAdam, LBAdamWBase, DSAdam
-from msamp.nn import LinearReplacer
+from msamp.nn import LinearReplacer, model_state
 from tests.helper import decorator
 
 
@@ -71,6 +71,32 @@ class LBAdamwTestCase(unittest.TestCase):
             opt2.zero_grad()
 
         self.assertTrue(torch.allclose(model1.weight, model2.weight.float(), 0, diff))
+
+    def test_all_reduce_grads(self):
+        from msamp.common.tensor import TensorDist
+        """Test the function `all_reduce_grads`."""
+        input = torch.randn(4, 4, device='cuda')
+        model1 = torch.nn.Linear(4, 4).cuda()
+        model2 = torch.nn.Linear(4, 4).cuda()
+        model1 = LinearReplacer.replace(model1, Dtypes.kfloat16)
+        model2 = LinearReplacer.replace(model2, Dtypes.kfloat16)
+        opt = LBAdamW(list(model1.parameters()) + list(model2.parameters()))
+        loss = (model1(input) + model2(input)).sum()
+        loss.backward()
+        old_all_reduce_avg = TensorDist.all_reduce_avg
+        num_grads = 0
+        def debug_all_reduce_avg(grads):
+            nonlocal num_grads
+            num_grads += len(grads)
+            return old_all_reduce_avg(grads)
+        TensorDist.all_reduce_avg = debug_all_reduce_avg
+        opt.all_reduce_grads(model1)
+        self.assertEqual(num_grads, 1)
+        self.assertFalse(model_state.ready_to_all_reduce_grads)
+        opt.all_reduce_grads(model2)
+        self.assertEqual(num_grads, 2)
+        self.assertFalse(model_state.ready_to_all_reduce_grads)
+        TensorDist.all_reduce_avg = old_all_reduce_avg
 
     def check_optimizer_state_dict(self, lbadam_class):
         """Save and load state dict of lbadam_class optimizer and check if the value is excepted.
