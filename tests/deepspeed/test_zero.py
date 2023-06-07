d854917ff890b52eda66a7375d41228117fb98d9
JackieWu <wkcn@live.cn>
2023-06-07 17:18:09 +0800

    feat(deepspeed): Deepspeed ZeRO Optimizer with MS-AMP support  (#73)
    **Description**
    In this PR, ZeRO Optimizer with MS-AMP support is integarted into
    `msamp.deepspeed`.
    
    **Major Revision**
    -  add deepspeed ZeRO optimizer with MS-AMP support
    -  add ut for zero
    - Upgrade pip in docker file.
    
    **Test in internal code**
    In FP8Train, run the following script and get around 28.3% top-1
    accuracy.
    ```python
    python -m torch.distributed.launch --master_port 1253 --nproc_per_node 8 main.py --cfg configs/deit/deit_mini.yaml --data-path ./cifar100 --batch-size 128 --use-deepspeed --output ./cifar_checkpoints/test1 --opts TRAIN.USE_CHECKPOINT False DATA.DATASET CIFAR100 FP8.USE_FP8_LINEAR True TRAIN.ZERO_STAGE 2
    ```
    
    ---------
    
    Co-authored-by: tocean <yuxiangyang@microsoft.com>

---
+++ tests/deepspeed/test_zero.py
@@ -0,0 +1,76 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+"""Tests for deepspeed zero optimizer for fp8."""
+import unittest
+
+import torch
+import torch.nn as nn
+
+from msamp import deepspeed
+from msamp.common.dtype.dtypes import Dtypes
+from msamp.nn import LinearReplacer
+from msamp.optim import LBAdam
+from tests.helper import decorator
+
+
+class FP8DeepSpeedZeroOptimizerTestCase(unittest.TestCase):
+    """Test Fp8DeepSpeedZeroOptimizer."""
+    def setUp(self):
+        """Hook method for setting up the test fixture before exercising it."""
+        torch.manual_seed(1000)
+        pass
+
+    def tearDown(self):
+        """Hook method for deconstructing the test fixture after testing it."""
+        pass
+
+    def _check_zero(self, ds_config):
+        model = nn.Linear(4, 4, device='cuda')
+        model = LinearReplacer.replace(model, Dtypes.kfloat16)
+        optimizer = LBAdam(list(model.parameters()))
+        model, _, _, _ = deepspeed.initialize(model=model, optimizer=optimizer, config=ds_config)
+
+        inputs = []
+        num_inputs = 10
+        for _ in range(num_inputs):
+            inputs.append(torch.rand(4, 4, device='cuda'))
+
+        losses = []
+        epoches = 10
+        for _ in range(epoches):
+            total_loss = 0
+            for i in range(num_inputs):
+                output = model(inputs[i])
+                loss = output.sum()
+                model.backward(loss)
+                total_loss += loss.item()
+                model.step()
+            avg_loss = total_loss / 10
+            losses.append(avg_loss)
+
+        for i in range(epoches):
+            if i > 0:
+                assert losses[i] < losses[i - 1]
+
+    @decorator.cuda_test
+    def test_stage1(self):
+        """Test fp8 deepspeed zero-stage1."""
+        config = {
+            'train_batch_size': 1,
+            'zero_optimization': {
+                'stage': 1,
+            }
+        }
+        self._check_zero(config)
+
+    @decorator.cuda_test
+    def test_stage2(self):
+        """Test fp8 deepspeed zero-stage2."""
+        config = {
+            'train_batch_size': 1,
+            'zero_optimization': {
+                'stage': 2,
+            }
+        }
+        self._check_zero(config)
