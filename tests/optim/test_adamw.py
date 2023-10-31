6619b58cd88d44079d405826e72fbc8032ffcb34
wkcn <wkcn@live.cn>
2023-10-31 13:46:26 +0000

    remove duplicated segment

--- tests/optim/test_adamw.py
+++ tests/optim/test_adamw.py
@@ -35,7 +35,6 @@ class LBAdamwTestCase(unittest.TestCase):
         ]:
             kwargs = dict(exp_avg_dtype=exp_avg_dtype, exp_avg_sq_dtype=exp_avg_sq_dtype)
             self.check_optimizer_step(LBAdamWBase, **kwargs)
-            self.check_optimizer_step(LBAdamWBase, **kwargs)
             self.check_optimizer_step(LBAdamW, **kwargs)
             self.check_optimizer_step(LBAdam, **kwargs)
             self.check_optimizer_step(DSAdam, **kwargs)
