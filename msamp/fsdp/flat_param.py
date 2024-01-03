fix no gradient issue

msamp/fsdp/flat_param.py
@@ -421,20 +421,7 @@ class FlatParamHandle:
             for param_name, param in submodule.named_parameters(recurse=False):
                 if param not in params_set:
                     continue
-                original_shapes.append(param.shape)
-                padded = 0
-                scaling_param = None
 
-                if not isinstance(param, torch.Tensor):
-                    scaling_param = param
-                    data = param.value.view(-1)
-                    if data.numel() % 4 != 0:
-                        padded = 4 - data.numel() % 4 
-                        data = F.pad(data, (0, padded))
-
-                    data = data.view(dtype=torch.float32)
-                    param = torch.nn.Parameter(data)
-                    setattr(submodule, param_name, param)
                 if param in shared_param_memo:  # shared reference
                     prim_module, prim_module_name, prim_param_name = shared_param_memo[
                         param
@@ -483,13 +470,17 @@ class FlatParamHandle:
                     )
                     fqns.append(fqn)
 
-                    if scaling_param is not None:
-                        metas.append(scaling_param.meta)
-                        scaling_metas.append(scaling_param._scaling_metas)
+                    if hasattr(param, '_fp8') and param._fp8:
+                        metas.append(param._meta)
+                        paddeds.append(param._padded)
+                        original_shapes.append(param._original_shape)
+                        scaling_metas.append(param._scaling_metas)
+                       
                     else:
                         metas.append(None)
+                        paddeds.append(0)
+                        original_shapes.append(None)
                         scaling_metas.append(None)
-                    paddeds.append(padded)
 
         assert requires_grad is not None, (
             "Passed-in `params` were not found in the module tree\n"

msamp/fsdp/fully_sharded_data_parallel.py
@@ -407,6 +407,26 @@ class FullyShardedDataParallel(nn.Module, _FSDPState):
         _init_prefetching_state(self, backward_prefetch, forward_prefetch)
         _init_buffer_state(self, module)
 
+        for name, submodule in module.named_modules():
+            params_to_process = list(submodule.named_parameters(recurse=False))
+            for param_name, param in params_to_process:
+                if not isinstance(param, torch.Tensor):
+                    data = param.value.view(-1)
+                    padded = 0
+                    if data.numel() % 4 != 0:
+                        padded = 4 - data.numel() % 4 
+                        data = torch.nn.functional.pad(data, (0, padded))
+
+                    data = data.view(dtype=torch.float32)
+                    new_param = torch.nn.Parameter(data)
+                    new_param._fp8 = True
+                    new_param._original_shape = param.shape
+                    new_param._padded = 0
+                    new_param._meta = param.meta
+                    new_param._scaling_metas = param._scaling_metas
+
+                    setattr(submodule, param_name, new_param)
+
         _init_param_handle_from_module(
             self,
             module,
@@ -759,7 +779,6 @@ class FullyShardedDataParallel(nn.Module, _FSDPState):
                         param._meta = self._flat_param._metas[i]
                         param._padded = self._flat_param._paddeds[i]
                         param._original_shape = self._flat_param._original_shapes[i]
-                        # setattr(submodule, param_name, param)
                     i += 1
 
             output = self._fsdp_wrapped_module(*args, **kwargs)

msamp/nn/functional.py
@@ -30,13 +30,13 @@ class _FP8GemmFunction(torch.autograd.Function):
             padded = weight._padded
             original_shape = weight._original_shape
             meta = weight._meta
-            ctx.original_weight = weight
 
             weight = weight.view(dtype=torch.uint8)
             if padded != 0:
                 weight = weight[0: weight.numel() - padded]
             weight = weight.view(original_shape)
             weight = ScalingParameter(ScalingTensor(weight, meta))
+            ctx._fp8 = True
 
         ctx.metas = metas
         model_state.check_metas_in_flat(metas)
@@ -109,9 +109,10 @@ class _FP8GemmFunction(torch.autograd.Function):
                 )
                 del old_wgrad
 
-            if hasattr(ctx, 'original_weight') and ctx.original_weight != None:
+            if ctx._fp8:
                 wgrad = wgrad.cast(Dtypes.kfloat8_e4m3, meta=wgrad_meta, sync=True)
-                ctx.original_weight.grad = wgrad.value.view(-1).view(dtype=torch.float32)
+                wgrad = wgrad.value.view(-1).view(dtype=torch.float32)
+                return input_grad, wgrad, None, None 
             else:
                 if model_state.use_fp8_ddp:
                     wgrad.meta = wgrad_meta