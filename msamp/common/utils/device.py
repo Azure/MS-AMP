f8150598909d74ca6c63da9dfd4198ec272c3e21
Wen Chen <Wen.Chen@amd.com>
2024-03-11 09:12:24 +0000

    fix ut bugs

--- msamp/common/utils/device.py
+++ msamp/common/utils/device.py
@@ -15,23 +15,10 @@ class GPUType(Enum):
 class Device:
     """Device class for different hardwares."""
     @staticmethod
-    def is_fp8_supported():
-        """Check whether the device support FP8 or not.
-
-        Return:
-            boolean: return True if the device support FP8 precision.
-        """
-        gpu_name = torch.cuda.get_device_name().lower()
-        if 'h100' in gpu_name:
-            return True
-
-        return False
-
-    @staticmethod
     def get_gpu_type():
         """Get the GPU type."""
         if torch.cuda.device_count() > 0:
-            device_name = torch.cuda.get_device_name(0)
+            device_name = torch.cuda.get_device_name(0).upper()
             if "NVIDIA" in device_name:
                 return GPUType.NVIDIA
             elif "AMD" in device_name:
